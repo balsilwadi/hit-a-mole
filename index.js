1e813d2b3ea01d893b0ed131dcdb0a157d2c0e49
Bilal Alsilwadi <balsilwadi@gmail.com>
2023-05-17 17:42:34 -0500

    hit a mole

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // your code 
+const holes = document.getElementsByClassName('hole')
+
+let score = 0
+const gameArea = document.getElementById('hit-a-mole');
+gameArea.addEventListener('click', function (clickEvent) {
+    const randomHoleIndex = Math.floor(Math.random() * holes.length);
+    holes[randomHoleIndex].classList.add("mole")
+  if (clickEvent.target.matches('.mole')) {
+    // we hit a mole!
+    score++;
+    document.querySelector('h2').innerHTML = score
+    
+    clickEvent.target.classList.remove("mole")
+    holes[randomHoleIndex].classList.add("mole")
+    
+  }
+  
+});
